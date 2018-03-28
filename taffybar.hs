12bd1cc7d199fea0274ecf9894b6f33a7d59d019
the_next <543584502@qq.com>
2018-03-28 12:17:34 +0800

    增加bar的工作区和电池显示

--- taffybar.hs
+++ taffybar.hs
@@ -4,15 +4,13 @@ import System.Information.CPU
 import System.Information.Memory
 import System.Taffybar
 import System.Taffybar.FreedesktopNotifications
-import System.Taffybar.MPRIS
 import System.Taffybar.SimpleClock
 import System.Taffybar.Systray
-import System.Taffybar.TaffyPager()
 import System.Taffybar.Weather
-import System.Taffybar.Widgets.PollingBar()
 import System.Taffybar.Widgets.PollingGraph
-import System.Taffybar.WorkspaceHUD
+import System.Taffybar.TaffyPager
 import System.Taffybar.Battery
+import System.Taffybar.MPRIS
 memCallback :: IO [Double]
 memCallback = do
   mi <- parseMeminfo
@@ -23,10 +21,6 @@ cpuCallback = do
   (userLoad, systemLoad, totalLoad) <- cpuLoad
   return [totalLoad, systemLoad,userLoad]
 
--- userCpuCallback :: IO [Double]
--- userCpuCallback = do
---   (userLoad, systemLoad, totalLoad) <- cpuLoad
---   return [totalLoad, systemLoad]
 main :: IO ()
 main = do
   let memCfg = defaultGraphConfig { 
@@ -40,15 +34,14 @@ main = do
         , graphLabel = Just "cpu"
       }
   let clock = textClockNew Nothing "<span fgcolor='orange'>%a %b %_d %H:%M</span>" 1
-      -- pager = hudFromPagerConfig defaultWorkspaceHUDConfig
+      pager = taffyPagerHUDLegacy defaultPagerConfig
       note = notifyAreaNew defaultNotificationConfig
       battery = batteryBarNew defaultBatteryConfig 10
-      wea = weatherNew (defaultWeatherConfig "KMSN") 10
       mpris = mprisNew defaultMPRISConfig
       mem = pollingGraphNew memCfg 1 memCallback
       cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
       tray = systrayNew
-  defaultTaffybar defaultTaffybarConfig { startWidgets = [  note ]
-                                        , endWidgets = [ tray, battery ,wea, clock, mem, cpu, mpris ]
+  defaultTaffybar defaultTaffybarConfig { startWidgets = [  pager,note ]
+                                        , endWidgets = [ tray, battery , clock, mem, cpu, mpris ]
                                         , barHeight     = 18
                                         }
