3ced0529f5b0194d9750b838d96c8f32b5d87c24
the_next <543584502@qq.com>
2018-04-01 19:30:45 +0800

    修改了cpu和内存用量的显示方式

--- taffybar.hs
+++ taffybar.hs
@@ -8,12 +8,29 @@ import System.Taffybar.Battery                      (batteryBarNew)
 import System.Taffybar.MPRIS                        (mprisNew,defaultMPRISConfig)
 import System.Taffybar.NetMonitor                   (netMonitorMultiNewWith)
 import System.Taffybar.Widgets.VerticalBar          (defaultBarConfig,BarConfig(..))
+import System.Taffybar.Widgets.PollingLabel         (pollingLabelNew) 
+import Text.Printf                                  (printf)
+import qualified Graphics.UI.Gtk as Gtk
 import System.Taffybar
-import System.Taffybar.Widgets.PollingGraph
 memCallback :: IO [Double]
 memCallback = do
   mi <- parseMeminfo
   return [memoryUsedRatio mi]
+
+toInt :: Double -> Int
+toInt x = round x
+
+makeColor :: [Double]-> String
+makeColor color = printf "#%02x%02x%02x" (toInt(color!!0 * 255)) (toInt(color!!1 * 255)) (toInt(color!!2 * 255))
+
+computerInfo ::IO String
+computerInfo = do
+  cpu <- (getCPULoad "cpu")
+  mem <- memCallback
+  let
+    cpuBar = "<span font='monospace 10' fgcolor='"++(makeColor [cpu!!0,1.0-cpu!!0,0.0])++"'>cpu"  ++ (printf "%02.0f"(cpu !! 0 * 100)) ++ "%</span>"
+    memBar = "<span font='monospace 10' fgcolor='"++(makeColor [mem!!0,1.0-mem!!0,0.0])++"'>mem"  ++ (printf "%02.0f"(mem !! 0 * 100)) ++ "%</span>"
+  return (cpuBar ++ "\n" ++ memBar)
 
 batteryConfig :: BarConfig
 batteryConfig =
@@ -23,26 +40,18 @@ batteryConfig =
 
 main :: IO ()
 main = do
-  let memCfg = defaultGraphConfig {
-        graphDataColors = [(1, 0, 0, 1)]
-          , graphLabel = Just "mem"
-      }
-      cpuCfg = defaultGraphConfig {
-        graphDataColors = [(0, 1, 0, 1)
-          , (1, 0, 1, 1)
-        ]
-        , graphLabel = Just "cpu"
-      }
-  let clock = textClockNew Nothing "<span font='9'  size='larger' fgcolor='#fff'>%m月%d日\n周%u %H:%M</span>" 30
+  let clock = textClockNew Nothing "<span font='monospace 9' fgcolor='#fff'>%m月%d日\n周%u %H:%M</span>" 30
       pager = taffyPagerHUDLegacy defaultPagerConfig
-      network = netMonitorMultiNewWith 1 ["enp4s0f1", "wlp3s0"] 3 "<span font='9' size='larger' fgcolor='#fff'>▼ $inKB$kb/s\n▲ $outKB$kb/s</span>"
+      network = netMonitorMultiNewWith 1 ["enp4s0f1", "wlp3s0"] 2 "<span font='monospace 10' fgcolor='#fff'>▼ $inKB$kb/s\n▲ $outKB$kb/s</span>"
       note = notifyAreaNew defaultNotificationConfig
       battery = batteryBarNew batteryConfig 10
       mpris = mprisNew defaultMPRISConfig
-      mem = pollingGraphNew memCfg 1 memCallback
-      cpu = pollingGraphNew cpuCfg 0.5 $ getCPULoad "cpu"
+      info = do
+        l <- pollingLabelNew "233" 1 computerInfo
+        Gtk.widgetShowAll l
+        return $ Gtk.toWidget l
       tray = systrayNew
   defaultTaffybar defaultTaffybarConfig { startWidgets = [  pager,note ]
-                                        , endWidgets = [ tray,network, battery , clock, mem, cpu, mpris ]
+                                        , endWidgets = [ tray,network,battery,clock,info,mpris]
                                         , barHeight     = 32
                                         }
