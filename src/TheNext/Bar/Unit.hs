efc23e453b2638196c6908839bf48bc944567522
the_next <543584502@qq.com>
2018-04-12 14:05:41 +0800

    完善组件代码

--- src/TheNext/Bar/Unit.hs
+++ src/TheNext/Bar/Unit.hs
@@ -1,26 +1,43 @@
 module TheNext.Bar.Unit (
+    addConfig,
+    unitBase
 ) where
 
 import qualified Graphics.UI.Gtk as Gtk
 import System.Taffybar.Widgets.PollingLabel         (pollingLabelNew) 
+import Text.Printf                                  (printf)
 
-data UnitConfig = UnitConfig{ fontColor :: (Int,Int,Int)
-                            , fontSize :: Int
-                            , backgroundColor :: (Int,Int,Int)
-                            , font :: String
+data UnitConfig = UnitConfig{ fontColor :: IO (Int,Int,Int)
+                            , fontSize :: IO Int
+                            , backgroundColor ::IO (Int,Int,Int)
+                            , font :: IO String
                             }
-unitBase ::IO String -> IO Gtk.Widget 
-unitBase content = do
-    l <- pollingLabelNew "waitForInit" 1 content
-    Gtk.widgetShowAll l
-    return $ Gtk.toWidget l 
-
-unitBase :: IO String -> UnitConfig -> IO Gtk.Widget
-unitBase content config = do 
+unitBase :: IO String -> IO Gtk.Widget
+unitBase content = do 
     l <- pollingLabelNew "waitForInit" 1 content
     Gtk.widgetShowAll l
     return $ Gtk.toWidget l 
 
 
-addConfig :: IO String -> UnitConfig -> IO String
-addConfig content config = "<span font='" ++ config.font ++ " " ++ config.fontSize ++ "' fgcolor='"++makeColor [head cpu,1.0-head cpu,0.0]++"'>cpu"  ++ "test"++ "%</span>"
+addConfig :: IO String -> (IO String -> IO UnitConfig) -> IO String
+addConfig content configFun = do
+    config <- configFun content
+    input <- content
+    myfont <- font config
+    myfontSize <- fontSize config 
+    (fr,fb,fg) <- fontColor config
+    return ("<span font='" ++ 
+        myfont ++ " "   ++ 
+        printf "%d" myfontSize ++ "' fgcolor='"++ 
+        makeColor (fr,fg,fb)++ "'>"  ++ input ++ "%</span>")
+
+makeColor :: (Int,Int,Int) -> String
+makeColor (r,g,b) = printf "#%02x%02x%02x" r b g
+
+defaultConfig :: IO String -> IO UnitConfig
+defaultConfig string = return UnitConfig{
+    fontColor = return (0,0,0)
+    , fontSize = return 9
+    , backgroundColor = return (0,0,0)
+    , font = return "monospace"
+}
