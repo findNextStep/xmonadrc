d97ec7c275a45eaa2b71f79c881cec7bf686328e
the_next <543584502@qq.com>
2018-04-17 20:19:21 +0800

    添加taffybar的风格配置

--- script/buildUp.sh
+++ script/buildUp.sh
@@ -3,5 +3,6 @@ cabal install &&
 echo "-- cabal buil up" && 
 xmonad --recompile && 
 echo "-- xmonad compile finsh" &&
+cp ~/.xmonad/taffybar.rc ~/.config/taffybar/
 echo "-- set up over" || echo "-- fail"
 
