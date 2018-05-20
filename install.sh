0b22efdffdadeff2d80e7f14773b02019967b467
the_next <543584502@qq.com>
2018-05-20 20:26:04 +0800

    更新依赖列表

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 apt install xmonad cabal dmenu feh taffybar hsetroot xcompmgr numlockx libgtk-3-dev indicator-cpufreq
+# haskell depend
+apt install gobject-introspection libgirepository1.0-dev libdbusmenu-gtk3-dev
+sudo apt-get install libgirepository1.0-dev libwebkit2gtk-4.0-dev libgtksourceview-3.0-dev
+
 # 我想要一劳永逸地解决启动资源管理器就启动gnome整个桌面系统的问题
 # 所以使用nemo
 sudo apt install dconf-tools nemo
