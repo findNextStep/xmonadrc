4ec4ba470e40fe9ac6ef004bcae6c4734f570c3b
findNextStep <543584502@qq.com>
2018-06-24 19:53:45 +0800

    添加如果没有放在xmonad默认位置的处理方式

--- script/buildUp.sh
+++ script/buildUp.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+# 安装自定义库
 cabal install && 
 echo "-- cabal buil up" && 
+# 如果不是xmoand配置文件夹，复制文件
+if [ $(pwd) != ~/.xmonad ];then 
+cp ./xmonad.hs ~/.xmonad && 
+cp ./xmonad-session-rc ~/.xmonad
+fi &&
+echo "-- xmonad copy successed" && 
+# 检查编译是否存在问题
 xmonad --recompile && 
 echo "-- xmonad compile finsh" &&
+# 复制taffybar文件
 cp ~/.xmonad/resources/taffybar.rc ~/.config/taffybar/ && 
 cp ~/.xmonad/resources/taffybar.css ~/.config/taffybar/ &&
 echo "-- set up over" || echo "-- fail"
