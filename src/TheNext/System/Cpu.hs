fa34dc502aeeaf1a517967afa40889fddc5d78b8
findNextStep <543584502@qq.com>
2018-06-26 16:52:53 +0800

    更改cpu负载获取方式

--- src/TheNext/System/Cpu.hs
+++ src/TheNext/System/Cpu.hs
@@ -2,21 +2,38 @@ module TheNext.System.Cpu(
     getCpuLoads
 )where
 
-import System.Information.CPU2  (getCPULoad)
+import System.Information.StreamInfo(getLoad)
 import Data.List                (sort)
+import System.Directory         (getDirectoryContents)
 
-readCpuLoad i = do
-    load <- getCPULoad ("cpu" ++ show i)
-    return [head load + load !! 1]
+getCpuNum = do
+    num <- getDirectoryContents "/sys/bus/cpu/devices"
+    return $ length num - 2
 
-loop 0 = readCpuLoad 0
+forAll ::(a -> b) -> [a] ->  [b]
+forAll fun [] = []
+forAll fun a  =  fun (head a) : forAll fun (tail a)
 
-loop a = do
-    sub <- loop (a-1)
-    this <- readCpuLoad a 
-    return $ sub ++ this
+toInt = forAll (read ::String -> Integer)
+
+cpuActive = do
+    cpu <- readFile "/proc/stat"
+    return $forAll sum(                         -- 求和
+            forAll toInt(                       -- 转换为整数形式
+            forAll(take 3)(                     -- 取前三个数
+            forAll tail(                        -- 去掉开头的cpu标号
+            forAll words                        -- 按空格分词
+            (filter(\x -> take 3 x =="cpu") $   -- 选取包含cpu工作时间的行
+            lines cpu)))))                      -- 分行
+   
+cpuTotal = do
+    cpu <- readFile "/proc/stat"
+    return $forAll sum(                         -- 求和
+            forAll toInt(                       -- 转换为整数形式
+            forAll tail(                        -- 去掉开头的cpu标号
+            forAll words                        -- 按空格分词
+            (filter(\x -> take 3 x =="cpu") $   -- 选取包含cpu工作时间的行
+            lines cpu))))                       -- 分行
 
 getCpuLoads::IO[Double]
-getCpuLoads = do
-    cpus <- loop 7
-    return $ sort cpus
+getCpuLoads = getLoad 0.05 cpuActive
