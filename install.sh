002a4e194f24e79bd753c6d14c16e21eec3ab908
findNextStep <findNextStep@gmail.com>
2018-07-15 00:07:25 +0800

    添加安装chrome

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
+# 添加vscode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-apt install xmonad cabal-install feh taffybar hsetroot xcompmgr numlockx libgtk-3-dev indicator-cpufreq suckless-tools sakura gobject-introspection libgirepository1.0-dev libdbusmenu-gtk3-dev libgirepository1.0-dev libwebkit2gtk-4.0-dev libgtksourceview-3.0-dev -y
+
+# 添加chrome
+sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+
+
+apt install google-chrome-stable code xmonad cabal-install feh taffybar hsetroot xcompmgr numlockx libgtk-3-dev indicator-cpufreq suckless-tools sakura gobject-introspection libgirepository1.0-dev libdbusmenu-gtk3-dev libgirepository1.0-dev libwebkit2gtk-4.0-dev libgtksourceview-3.0-dev -y
 
 # 我想要一劳永逸地解决启动资源管理器就启动gnome整个桌面系统的问题
 # 所以使用nemo
