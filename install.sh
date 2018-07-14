cf8957a68ff14bfe8bd9a60aad4aa75a0cfea790
findNextStep <findNextStep@gmail.com>
2018-07-15 00:01:03 +0800

    添加vscode的下载条件

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 apt install xmonad cabal-install feh taffybar hsetroot xcompmgr numlockx libgtk-3-dev indicator-cpufreq suckless-tools sakura gobject-introspection libgirepository1.0-dev libdbusmenu-gtk3-dev libgirepository1.0-dev libwebkit2gtk-4.0-dev libgtksourceview-3.0-dev -y
 
 # 我想要一劳永逸地解决启动资源管理器就启动gnome整个桌面系统的问题
