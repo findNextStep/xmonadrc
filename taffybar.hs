744d3df1e915d28ae5ef4c73a13cdc675893be57
findNextStep <findNextStep@gmail.com>
2018-07-30 10:02:20 +0800

    修改taffybar字体

--- taffybar.hs
+++ taffybar.hs
@@ -24,11 +24,13 @@ cpuCallback = do
   (userLoad, systemLoad, totalLoad) <- cpuLoad
   return [totalLoad, systemLoad]
 
+font = "ubuntu mono bold 12"
+
 setFontSize size str = "<span font=\'" ++ size ++ "\'>" ++ str ++ "</span>"
 
 main = do
   let memCfg = defaultGraphConfig { graphDataColors = [(1, 1, 1, 1)]
-                                  , graphLabel = Just "mem"
+                                  , graphLabel = Just $ setFontSize font "mem"
                                   , graphBackgroundColor = (18/255,33/255,52/255)
                                   , graphBorderColor = (18/255,23/255,52/255)
                                   }
@@ -36,25 +38,25 @@ main = do
                                                       , (1, 1, 1, 0.5)
                                                       ]
                                   , graphBackgroundColor = (18/255,33/255,52/255)
-                                  , graphLabel = Just "cpu"
+                                  , graphLabel = Just $ setFontSize font "cpu"
                                   , graphBorderColor = (18/255,23/255,52/255)
                                   }
-  let clock = textClockNew Nothing "<span>%F %T</span>" 1
-      pager = taffyPagerNew defaultPagerConfig { activeWindow     = setFontSize "9" . colorize "#fff" "" . escape
-                                               , activeLayout     = escape                                 
-                                               , activeWorkspace  = wrap "-> " "" . escape
-                                               , hiddenWorkspace  = colorize "#888" "" . escape
+  let clock = textClockNew Nothing (setFontSize font "<span>%F %T</span>") 1
+      pager = taffyPagerNew defaultPagerConfig { activeWindow     = setFontSize font . colorize "#fff" "" . escape
+                                               , activeLayout     = setFontSize font . escape                                 
+                                               , activeWorkspace  = setFontSize font . wrap "-> " "" . escape
+                                               , hiddenWorkspace  = setFontSize font . colorize "#888" "" . escape
                                                , emptyWorkspace   = const ""
-                                               , visibleWorkspace = colorize "#bbb" "" . wrap "(" ")" . escape
-                                               , urgentWorkspace  = colorize "red" "yellow" . escape
-                                               , widgetSep        = colorize "#325A8E" "" " | "
+                                               , visibleWorkspace = setFontSize font . colorize "#bbb" "" . wrap "(" ")" . escape
+                                               , urgentWorkspace  = setFontSize font . colorize "red" "yellow" . escape
+                                               , widgetSep        = setFontSize font (colorize "#325A8E" "" "|")
                                                }
       -- note = notifyAreaNew defaultNotificationConfig
       mpris = mprisNew defaultMPRISConfig
       mem = pollingGraphNew memCfg 1 memCallback
       cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
       tray = systrayNew
-      net = netMonitorNew 1 "wlp3s0"
+      net = netMonitorNewWith 1 "wlp3s0" 2 $ setFontSize font "▼$inKB$kb/s\t▲$outKB$kb/s\t"
   taffybarMain defaultTaffybarConfig { startWidgets = [ pager ] -- , note ]
                                         , endWidgets = [ tray, clock, mem, cpu, net ]
                                         , barPosition = Bottom
