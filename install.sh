cbb1ec21e9bbd5dda5bedb66b035292289f1058e
findNextStep <findNextStep@gmail.com>
2019-04-12 14:49:16 +0800

    添加命令行fan

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo add-apt-repository ppa:daniruiz/flat-remix
 
 apt update
 
-apt install copyq mpd astyle tty-clock ipython gnome-screensaver screen tree polipo libboost1.62-all-dev libclang-common-6.0-dev libclang-dev libclang1-5.0 libclang-5.0-dev ycmd xbuilder fcitx-table fcitx-bin powertop haskell-stack haskell-platform lua5.1 ruby lua5.1-policy-dev perl libperl-dev python3 python3.6-dev libruby2.5 ruby-dev llvm clang ack libclang-6.0-dev gimp htop xclip arandr hsetroot xcompmgr shutter indicator-cpufreq autocutsel numlockx happy alex icdiff xmonad google-chrome-stable code xmonad cabal-install feh taffybar xcompmgr numlockx libgtk-3-dev indicator-cpufreq suckless-tools sakura gobject-introspection libgirepository1.0-dev libdbusmenu-gtk3-dev libgirepository1.0-dev libwebkit2gtk-4.0-dev libgtksourceview-3.0-dev python-smbus python3-pip -y lxappearance flat-remix-gnome
+apt install privoxy copyq mpd astyle tty-clock ipython gnome-screensaver screen tree polipo libboost1.62-all-dev libclang-common-6.0-dev libclang-dev libclang1-5.0 libclang-5.0-dev ycmd xbuilder fcitx-table fcitx-bin powertop haskell-stack haskell-platform lua5.1 ruby lua5.1-policy-dev perl libperl-dev python3 python3.6-dev libruby2.5 ruby-dev llvm clang ack libclang-6.0-dev gimp htop xclip arandr hsetroot xcompmgr shutter indicator-cpufreq autocutsel numlockx happy alex icdiff xmonad google-chrome-stable code xmonad cabal-install feh taffybar xcompmgr numlockx libgtk-3-dev indicator-cpufreq suckless-tools sakura gobject-introspection libgirepository1.0-dev libdbusmenu-gtk3-dev libgirepository1.0-dev libwebkit2gtk-4.0-dev libgtksourceview-3.0-dev python-smbus python3-pip -y lxappearance flat-remix-gnome
 
 # 我想要一劳永逸地解决启动资源管理器就启动gnome整个桌面系统的问题
 # 所以使用nemo
